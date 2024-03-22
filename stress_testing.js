e18a017684ea0bf83c4518c6d2561037d4763ca3
Abimbola Okusada <44692035+abimbola001@users.noreply.github.com>
2024-03-22 16:43:00 +0300

    Update stress_testing.js

--- stress_testing.js
+++ stress_testing.js
@@ -4,8 +4,8 @@ import { sleep } from 'k6';
 export const options = {
   // Key configurations for Stress in this section
   stages: [
-    { duration: '10m', target: 200 }, // traffic ramp-up from 1 to a higher 200 users over 10 minutes.
-    { duration: '30m', target: 200 }, // stay at higher 200 users for 30 minutes
+    { duration: '10m', target: 100 }, // traffic ramp-up from 1 to a higher 200 users over 10 minutes.
+    { duration: '30m', target: 100 }, // stay at higher 200 users for 30 minutes
     { duration: '5m', target: 0 }, // ramp-down to 0 users
   ],
 };
