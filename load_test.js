ef7b1cb516e230feec3cd57b14881da6c014b397
Abimbola Okusada <44692035+abimbola001@users.noreply.github.com>
2024-03-23 09:40:44 +0300

    Update load_test.js

--- load_test.js
+++ load_test.js
@@ -4,9 +4,14 @@ import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporte
 export const options = {
   thresholds: {
     http_req_duration: ["p(95)<150"],
+    http_req_failed: ['rate<0.1'],     // Error rate should be less than 10%
   },
   vus: 10,
-  duration: "10s",
+  stages: [
+    { duration: '1m', target: 50 },  // Ramp up to 50 virtual users over 1 minute
+    { duration: '3m', target: 50 },  // Stay at 50 virtual users for 3 minutes
+    { duration: '1m', target: 0 },   // Ramp down to 0 virtual users over 1 minute
+  ],
 };
 
 export default function () {
